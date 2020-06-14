e7047dbe4f1a141d9d9307dbc182766874b9a7fd
zhang-alvin <alv.zhang@gmail.com>
2020-06-13 22:38:07 -0400

    CASE: shear tank

---
+++ shearTank/ls_p.py
@@ -0,0 +1,28 @@
+from proteus import *
+from proteus.default_p import *
+from dambreak import *
+from proteus.mprans import NCLS
+from proteus import Context
+ct = Context.get()
+
+#parallelPeriodic=True
+LevelModelType = NCLS.LevelModel
+
+coefficients = NCLS.Coefficients(V_model=0,RD_model=3,ME_model=2,
+                                 checkMass=False, useMetrics=useMetrics,
+                                 epsFact=epsFact_consrv_heaviside,sc_uref=ls_sc_uref,sc_beta=ls_sc_beta,movingDomain=movingDomain)
+ 
+def getDBC_ls(x,flag):
+    return None
+
+dirichletConditions = {0:getDBC_ls}
+
+advectiveFluxBoundaryConditions =  {}
+diffusiveFluxBoundaryConditions = {0:{}}
+
+class PerturbedSurface_phi:       
+    def uOfXT(self,x,t):
+        return signedDistance(x)
+    
+initialConditions  = {0:PerturbedSurface_phi()}
+#periodicDirichletConditions = {0:ct.getPDBC}
